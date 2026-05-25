79bab2d785e400796d0e6f96b721e4924d42ca5e
Zxwray <winidimas992@gmail.com>
2026-05-25 16:15:45 +0700

    aku adalah deploy

--- siswa-app/src/services/api.js
+++ siswa-app/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://synau-backend-579679620696.us-central1.run.app/api',
     headers: { 'Content-Type': 'application/json' }
 })
 
